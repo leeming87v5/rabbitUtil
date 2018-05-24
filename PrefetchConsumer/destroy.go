2c5f8793fabda7ae6c1754bb3477b0da8dbf4034
leeming87v5 <1419912022@qq.com>
2018-05-24 10:23:27 +0800

    Update destroy.go

--- PrefetchConsumer/destroy.go
+++ PrefetchConsumer/destroy.go
@@ -1,6 +1,6 @@
 package PrefetchConsumer
 
-import "destroyer"
+import "github.com/leeming87v5/rabbitUtil/destroyer"
 
 // Destroy instance of Destroyer
 // If GOGC=off you should call obj.Destroy() manually
